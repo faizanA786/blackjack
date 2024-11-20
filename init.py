20b40f0c2367147fd0851ad344c1806821cec1fb
outlaw16 <faizan65650@gmail.com>
2024-11-20 14:57:37 +0000

    chore: Initial commit

--- init.py
+++ init.py
@@ -6,11 +6,33 @@ This is called upon launching the program.
 """
 
 # dependencies
+from tkinter import *
 from userstats import dbInit, newProfile, login 
 import main_menu
 import game
 
 def init(): # initialises the program
+    WIDTH = 1000
+    HEIGHT = 500
+    main = Tk()
+    main.geometry(str(WIDTH) + "x" + str(HEIGHT))
+    main.config(bg="black")
+    main.title("Main Menu")
+
+    menu = Menu(main)
+    main.config(menu=menu)
+
+    header = Label(main, text="Blackjack!", font=("Arial", 40), bg="black", fg="white")
+    #createProfile = Button(main, text="New Profile", font=("Arial", 30), bg="black", fg="white", command=)
+    #login = Button(main, text="Login", font=("Arial", 30), bg="black", fg="white", command=)
+    exit = Button(main, text="Exit", font=("Arial", 30), bg="black", fg="white", command=main.destroy)
+
+    header.place(relx=0.5, rely=0.2, anchor=CENTER)
+    #createProfile.place()
+    #login.place()
+    exit.place(relx=0.5, rely=0.6, anchor=CENTER)
+    main.mainloop()
+
     dbInit()
     nav = int(input("Login (0)\nNew Account (1)\nYou: "))
     signedIn = False
